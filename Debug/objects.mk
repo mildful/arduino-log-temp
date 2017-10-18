9bb6d8290306d8f8e02e5c577be0f981732fe08a
Cl�ment Flodrops <clement.flodrops@gmail.com>
2017-10-18 10:53:15 +0200

    rm debug

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -Wl,-T"C:/ti/tirex-content/energia-0101E0017/hardware/cc3200/cores/cc3200/cc3200.ld" -llpcc3200_MQTTClient -llpcc3200_Adafruit_TMP006 -llpcc3200_WiFi -llpcc3200_Wire -llpcc3200_core -ldriverlib -lm -lc -lgcc
-
